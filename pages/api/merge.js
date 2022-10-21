388305fdf14cc0aeeb918972d9582bf255bbeea4
Faisal <faisal.hoque@hotmail.com>
2022-10-21 23:12:14 +0100

    First commit

---
+++ pages/api/merge.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { PDFDocument } from 'pdf-lib';
+
+export default async function handler(req, res) {
+  const mergedPdf = await PDFDocument.create();
+
+  for (let document of JSON.parse(req.query.body.documentArray)) {
+    document = await PDFDocument.load(document);
+
+    const copiedPages = await mergedPdf.copyPages(document, document.getPageIndices());
+    copiedPages.forEach((page) => mergedPdf.addPage(page));
+  }
+
+  return res.status(200).json({ '$content-type': 'application/pdf', '$content': `${await mergedPdf.saveAsBase64() }`});
+}
+
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+};
+
